a024ba3ea9be90b4b9eb166477b386e849fcf646
Dorian Gonzalez <doriandarren1@gmail.com>
2024-12-23 21:30:12 +0100

    add react reference

---
+++ react/reference-guide.js
@@ -0,0 +1,45 @@
+
+/************************************************
+ * 
+ *  VARIABLES
+ * 
+ ************************************************/
+
+
+/* 
+
+Hook
+
+
+Se importa:
+import { useState } from "react";
+
+
+Luego en el componente:
+const [variable, setVariable] = useState('Hola mundo');      // Se puede inicializar con cualquier valor
+
+
+
+
+ 
+// Igual 
+const { variable, otraVariable } = props;  -> props.variable; 
+variable 
+otraVariable
+
+
+props.variable
+props.otraVariable
+
+
+-----------------------------------------------------
+--------  Template: <template>...</template> --------
+-----------------------------------------------------
+
+
+
+*/
+
+
+
+
