cf5739ff33dee0dd6ac382c2a2a3cc1aca115b16
Dorian Gonzalez <doriandarren1@gmail.com>
2024-12-23 21:31:31 +0100

    addd 2

--- react/reference-guide.js
+++ react/reference-guide.js
@@ -19,9 +19,6 @@ Luego en el componente:
 const [variable, setVariable] = useState('Hola mundo');      // Se puede inicializar con cualquier valor
 
 
-
-
-
  
 // Igual 
 const { variable, otraVariable } = props;  -> props.variable; 
