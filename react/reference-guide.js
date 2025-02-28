8d50ce8d598c8885eb016d4a1608bbd14eefc6d2
Dorian Gonzalez <doriandarren1@gmail.com>
2025-02-28 22:18:10 +0100

    refrence react

--- react/reference-guide.js
+++ react/reference-guide.js
@@ -8,15 +8,15 @@
 
 /* 
 
-Hook
-
+- useSate:
 
 Se importa:
 import { useState } from "react";
 
-
 Luego en el componente:
 const [variable, setVariable] = useState('Hola mundo');      // Se puede inicializar con cualquier valor
+
+
 
 
  
@@ -31,12 +31,46 @@ props.otraVariable
 
 
 -----------------------------------------------------
---------  Template: <template>...</template> --------
+--------   --------
 -----------------------------------------------------
+
+- Estructura de un JSX:
+
+import React from 'react'                   //<----- Aqui van los import 
+
+ 
+const formData = {                          //<----- (NO SIEMPRE) Van las declaraciones de las variables y/o constantes y/o funciones 
+    ...
+}
+
+
+export const reference-guide = () => {
+
+    const dispatch = useDispatch();         //<----- Aqui van los "use..." (Hook)
+
+
+    const formData = {                      //<----- (NO SIEMPRE) Van las declaraciones de las variables y/o constantes y/o funciones 
+        ...
+    }
+
+
+
+    return (
+        <div>reference-guide</div>          //<----- Aqui va el HTML y/o renderizacion y/o llamadas a componentes
+    )
+}
+
+
 
 
 
 */
+
+
+
+
+
+
 
 
 
