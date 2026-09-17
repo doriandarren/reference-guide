dbfab6d3c451636b0c8a2b677fdbe30623bc0ff6
Dorian Gonzalez <doriandarren1@gmail.com>
2026-09-17 23:41:09 +0200

    add swap

--- python/Curso-Python/swap.py
+++ python/Curso-Python/swap.py
@@ -1,4 +1,29 @@
 """
+Intercambio
+
+Dado un valor string, crea una nueva cadena donde se intercambien el primer y el último carácter. Hazlo en una sola línea de código (como máximo dos líneas si cuentas la declaración de la variable de cadena).
+
+Puedes asumir que la longitud mínima de la cadena es 2.
+
+Ejemplo 1
+
+Aporte:
+
+string: Python
+
+Producción:
+
+nythoP
+
+Ejemplo 2
+
+Aporte:
+
+string: hello
+
+Producción:
+
+oellh
 
 """
 
