587df0ef8f198b77fb08ad29d8ff4226bb1e7f52
Dorian <doriandarren1@gmail.com>
2026-09-21 13:42:26 +0200

    Enhance list_example.py with comments
    Added comments to clarify code functionality.

--- python/Curso-Python/list_example.py
+++ python/Curso-Python/list_example.py
@@ -1,4 +1,18 @@
 my_list = [1, 2, 3, 4, 5, 5, 6, 7, 8, 9]
 
-odd_indexed_elements = my_list[1::2] # impares de una lista
+
+# impares de una lista
+odd_indexed_elements = my_list[1::2]
 print(odd_indexed_elements)
+
+
+
+
+## First Half: el medio
+count = len(my_list)
+first_half = count // 2
+print(my_list[:first_half])
+
+
+## Dos ultimos elementos
+print(my_list[-2:])
